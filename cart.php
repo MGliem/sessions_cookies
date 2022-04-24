a7297edb26571eda1b8821e1baa1bd556c12ae4e
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-04-24 11:38:03 +0200

    fix when card is empty

--- cart.php
+++ cart.php
@@ -9,11 +9,13 @@ if (empty($_SESSION['name'])) {
 <?php require 'inc/head.php'; ?>
 <section class="cookies container-fluid">
     <div class="row">
+        <?php if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) :?>
         <ul>
             <?php foreach ($_SESSION['cart'] as $product) : ?>
                 <li><?= $product['name'] ?> x <?= $product['total'] ?></li>
             <?php endforeach; ?>
         </ul>
+        <?php endif;?>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
